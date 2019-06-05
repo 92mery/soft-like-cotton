7f9d497ba48318222b30f126442b48ec0b72cf36
92mery <48825822+92mery@users.noreply.github.com>
2019-06-05 17:24:26 +0200

    Add files via upload

---
+++ Simplon_Soft Like Cotton/js/script.js
@@ -0,0 +1,37 @@
+function afficherTextTete() {
+	document.getElementById("descriptif_tete").style.display = "block";
+}
+
+function afficherTextCoeur() {
+	document.getElementById("descriptif_coeur").style.display = "block";
+}
+
+function afficherTextBras() {
+	document.getElementById("descriptif_bras").style.display = "block";
+}
+
+function afficherTextJambe() {
+	document.getElementById("descriptif_jambe").style.display = "block";
+}
+
+function masquer_tete(){
+		document.getElementById("descriptif_tete").style.display = "none";
+
+}
+function masquer_coeur(){
+		document.getElementById("descriptif_coeur").style.display = "none";
+
+}
+
+function masquer_bras(){
+		document.getElementById("descriptif_bras").style.display = "none";
+
+}
+
+function masquer_jambe(){
+		document.getElementById("descriptif_jambe").style.display = "none";
+
+}
+
+
+
